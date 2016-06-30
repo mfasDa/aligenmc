c22741a83ad8dfdfd0c889648b155aeb77efe605
jkl <jochen.klein@cern.ch>
2016-06-30 15:39:04 +0200

    Add JEWEL

---
+++ jewel/gen.sh
@@ -0,0 +1,17 @@
+gen_jewel() {
+    case $1 in
+	run)
+	    PACKAGES="JEWEL::v2.0.2-3"
+
+	    sed -e "s,^\([[:space:]]*NEVENT\) .*$,\1 ${NEV}," \
+		-e "s,^\([[:space:]]*HEPMCFILE\) .*$,\1 ${HEPMCFILENAME}," \
+		$(dir_gen)/params-simple.dat > params.dat
+
+	    run_in_env jewel-2.0.2-vac params.dat > sim.log
+	    ;;
+
+	*)
+	    help "JEWEL"
+	    ;;
+    esac
+}
