bd65c0ce3846261a4f6ae932200ec6ec7e922730
ALICE PWG-MM <alipwgmm@lxplus109.cern.ch>
2016-10-13 13:20:59 +0200

    Fix for PYTHIA 6

--- gen/pythia6/gen.sh
+++ gen/pythia6/gen.sh
@@ -1,7 +1,7 @@
 gen_pythia6() {
     case $1 in
 	run)
-	    PACKAGES="pythia6/latest,AGILe/latest"
+	    PACKAGES="AGILe/v1.4.1-alice1-1,pythia6/428-1"
 
 	    run_in_env "agile-runmc Pythia6:HEAD --beams=LHC:${ENERGY} -n ${NEV} --out=${HEPMCFILENAME}"
 
