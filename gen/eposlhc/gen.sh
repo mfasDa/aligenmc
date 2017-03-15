c74abb967c30ccf6d323ed12a6ee37fbca750a7e
jkl <jochen.klein@cern.ch>
2017-03-15 10:21:55 +0100

    Allow for PbPb usage of EPOS-LHC

--- gen/eposlhc/gen.sh
+++ gen/eposlhc/gen.sh
@@ -3,8 +3,17 @@ gen_eposlhc() {
 	run)
 	    PACKAGES="CRMC::v1.5.4-3"
 
-	    run_in_env "sed -e s#__CRMC_BASEDIR__#\$CRMC_ROOT# $(dir_gen)/crmc_template.param > crmc.param"
-	    run_in_env crmc -o hepmc -p 6500 -P-6500 -n ${NEV} -m 0 -f ${HEPMCFILENAME} > sim.log
+	    case $TUNE in
+		pbpb)
+		    run_in_env "sed -e s#__CRMC_BASEDIR__#\$CRMC_ROOT# $(dir_gen)/crmc_template.param > crmc.param"
+		    run_in_env crmc -o hepmc -p $((ENERGY / 2)) -P-$((ENERGY / 2)) -i 822080 -I 822080 -n ${NEV} -m 0 -f ${HEPMCFILENAME} > sim.log
+		    ;;
+
+		*)
+		    run_in_env "sed -e s#__CRMC_BASEDIR__#\$CRMC_ROOT# $(dir_gen)/crmc_template.param > crmc.param"
+		    run_in_env crmc -o hepmc -p 6500 -P-6500 -n ${NEV} -m 0 -f ${HEPMCFILENAME} > sim.log
+		    ;;
+	    esac
 	    ;;
 
 	*)
