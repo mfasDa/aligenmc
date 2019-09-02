992e28a525aafe54ab7a0fd9f95b8180550a0c43
Jochen Klein <qgp@users.noreply.github.com>
2019-09-02 17:28:26 +0200

    Update gen.sh

--- gen/thepeg/gen.sh
+++ gen/thepeg/gen.sh
@@ -4,10 +4,10 @@ gen_thepeg() {
 	    PACKAGES="ThePEG::v2015-08-11-4"
 
 	    sed -e "s#^\([[:space:]]*set HepMCFile:Filename\) .*\$#\1 ${HEPMCFILENAME}#" $(dir_gen)/Rope.in > thepeg.in
-        cp_input LHCpp.in
+            cp_input LHCpp.in
 	    cp_input Tune31.in .
 
-        run_in_env 'setupThePEG -r ${THEPEG_ROOT}/lib/ThePEG/ThePEGDefaults.rpo -I ${THEPEG_ROOT}/share/Ariadne thepeg.in' > setup.log
+            run_in_env 'setupThePEG -r ${THEPEG_ROOT}/lib/ThePEG/ThePEGDefaults.rpo -I ${THEPEG_ROOT}/share/Ariadne thepeg.in' > setup.log
 	    run_in_env runThePEG Rope.run -N ${NEV} --seed ${SEED} > sim.log
 	    ;;
 
